33b89d06dc75205ac7f88eecbec10cc02a3b9ba5
muti <mutideveloper01@gmail.com>
2025-01-15 21:12:08 +0330

    upload project

---
+++ js/jvascript.js
@@ -0,0 +1,19 @@
+// function hide_sidebar(){
+//     const sidebar= document.querySelector('.sidebar');
+//     sidebar.style.display='none';
+// }
+
+// function show_sidebar(){
+//     const sidebar= document.querySelector('.sidebar');
+//     sidebar.style.display='flex';
+// }
+
+// function hide_box(){
+//     const login= document.querySelector('.box-signin');
+//     login.style.display='none';
+// }
+
+// function show_box(){
+//     const login= document.querySelector('.box-signup');
+//     login.style.display='flex';
+// }
